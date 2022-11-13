d38bb744b9b7b54301b0947b5265ac83f9951e61
Lambda-Recur <lambda.recur@gmail.com>
2022-11-13 10:23:31 -0500

    Rearranged and corrected flow logic of 'SimpleHack.ts' and recompiled.

--- SimpleHack.js
+++ SimpleHack.js
@@ -2,19 +2,15 @@
 
 async function main(ns) {
     const target = ns.args[0];
-    let stash = ns.getServerMoneyAvailable(target);
     const moneyThreshold = ns.getServerMaxMoney(target) * 0.90;
-    let security = ns.getServerSecurityLevel(target);
     const securityThreshold = ns.getServerMinSecurityLevel(target) + 5;
     const access = ns.hasRootAccess(target);
     while(access){
-        if (security > securityThreshold) {
+        if (ns.getServerSecurityLevel(target) > securityThreshold) {
             await ns.weaken(target);
-            security = ns.getServerSecurityLevel(target);
         } else if (await ns.grow(target) < 0.01) {
-            while(security > securityThreshold && stash > moneyThreshold){
+            while(ns.getServerSecurityLevel(target) > securityThreshold && ns.getServerMoneyAvailable(target) > moneyThreshold){
                 await ns.hack(target);
-                stash = ns.getServerMoneyAvailable(target);
             }
         }
     }
--- SimpleHack.ts
+++ SimpleHack.ts
@@ -3,21 +3,18 @@
 import { NS } from "./bitburner.d.ts"
 export async function main(ns: NS): Promise<void> {
   const target = <string>ns.args[0]
-  let stash = <number>ns.getServerMoneyAvailable(target)
   const moneyThreshold = <number>ns.getServerMaxMoney(target) * 0.90
-  let security = <number>ns.getServerSecurityLevel(target)
   const securityThreshold = <number>ns.getServerMinSecurityLevel(target) + 5
   const access = <boolean>ns.hasRootAccess(target)
 
   while (access) {
-    if (security > securityThreshold) {
+    if (<number>ns.getServerSecurityLevel(target) > securityThreshold) {
       await ns.weaken(target)
-      security = <number>ns.getServerSecurityLevel(target)
     }
     else if (await ns.grow(target) < 0.01) {
-      while ( security > securityThreshold && stash > moneyThreshold) {
+      while ( <number>ns.getServerSecurityLevel(target) > securityThreshold
+              && <number>ns.getServerMoneyAvailable(target) > moneyThreshold) {
         await ns.hack(target)
-        stash = <number>ns.getServerMoneyAvailable(target)
       }
     }
   }
